1696d22099b4933be51cad674250783697fa5ec8
Maurya, Ajay <amitmaury15893@gmail.com>
2020-07-11 18:07:02 +0530

    final

--- single-module/src/main/java/com/example/Greeter.java
+++ single-module/src/main/java/com/example/Greeter.java
@@ -14,6 +14,6 @@ public class Greeter {
 
   //TODO: Add javadoc comment
   public String greet(String Amit{
-    return String.format("Hello, %s!", Amit
+    return String.format("Hello, %s!", Amit)
   }
 }
